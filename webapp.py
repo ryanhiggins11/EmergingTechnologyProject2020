1c97eb275b39fc6561f5f3f2221acb0594c14cfe
ryanhiggins11 <46933572+ryanhiggins11@users.noreply.github.com>
2021-01-05 17:08:56 +0000

    Tried to Decrease output also added index file and got webservice running

--- webapp.py
+++ webapp.py
@@ -1,32 +1,24 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+# Flask web microservice import.
+from flask import Flask, request, render_template
+import tensorflow as tf
+
+# Create a new web app.
+app = Flask(__name__)
+
+# Setting a constant for the index page
+INDEX = "index.html"
+
+# Add route to the root ('/') location, that calls the "hello_world()" function.
+@app.route('/', methods=["GET", "POST"])
+def root():
+    if request.method == "POST":
+        # Setting a wind speed that we want to predict the power output for
+        speed = request.form["speed"]
+        # Casting wind_speed to a float from a string
+        speed = [float(speed)]
+        # Grabbing the saved wind turbine tensorflow model
+        Output_Model = tf.keras.models.load_model('Output_Model')
+        # Testing the accuracy of the predictions (using the "wind_speed" guess)
+        model_prediction = Output_Model.predict(speed)
+        return render_template(INDEX, prediction=model_prediction[0])
+    return render_template(INDEX)
